aa84a34a2ff64d0cf003a1950a8266f9582698db
Mei Dan Zhu <meidan.zhu@gmail.com>
2020-05-17 04:29:01 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,3 +1,11 @@
 class Anagram
+  attr_accesspr :word\
 
+  def initialize(word)
+    @word = word
+  end
+
+  def match(array)
+    array.select {|x| x.split("").sort == @word.split("").sort}
+  end
 end
